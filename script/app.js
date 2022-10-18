56e9559353f200892f58b0cfab2c4b8158656a76
Garry3Fingers <www.vlad78@yandex.ru>
2022-10-18 21:17:01 +0300

    Create variables for html buttons

---
+++ script/app.js
@@ -0,0 +1,23 @@
+// Const for buttons
+
+const outputPara = document.querySelector('p');
+
+const digitsBtn = document.querySelectorAll('.digits-btn');
+
+const clearBtn = document.querySelector('.clear-btn');
+
+const undoBtn = document.querySelector('.undo-btn');
+
+const divideBtn = document.querySelector('.divide-btn');
+
+const multiplicationBtn = document.querySelector('.multiplication-btn');
+
+const minusBtn = document.querySelector('.minus-btn');
+
+const plusBtn = document.querySelector('.plus-btn');
+
+const evenBtn = document.querySelector('.even-btn');
+
+const periodBtn = document.querySelector('.period-btn');
+
+
